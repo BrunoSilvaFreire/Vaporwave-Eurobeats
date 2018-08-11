f522f042faa4fdf1ec63e9e34e2fd91651faf24a
Ricardo <ricardonovak@yahoo.com.br>
2018-08-11 12:22:19 -0300

    Perlin Noise Generator
    FillChunk top

---
+++ Assets/Vaporwave&Eurobeats/Scripts/World/PerlinNoise.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinNoise {
+    
+  
+    public static float[,] Generate(int w, int h, int octaveCount, int seed){
+
+        float[,] baseNoise = GenerateWhiteNoise(w, h, seed);
+        int width = baseNoise.GetLength(0);
+        int height = baseNoise.GetLength(1);
+
+        //Um array de matrizes (geralmente usando 8, no final cada uma dessas matrizes será somada para gerar a imagem final
+        float[,,] smoothNoise = new float[octaveCount, width, height];
+
+        float persistence = 0.50f;
+
+        for (int i = 0; i < octaveCount; i++){
+            //cada loop do for gera uma nova matriz mais suave que a anteior, todas baseadas na primeira matriz de noise gerada no programa
+            var smoothMatrix = GenerateSmoothNoise(baseNoise, i);
+            for (int x = 0; x < smoothMatrix.GetLength(0); x++) {
+                for (int y = 0; y < smoothMatrix.GetLength(1); y++) {
+                    smoothNoise[i, x, y] = smoothMatrix[x, y];
+                }
+            }      
+        }
+        
+        float[,] perlinNoise = new float[width,height];
+        float amplitude = 1.0f;
+        float totalAmplitude = 0.0f;
+
+        //For feito para somar cada uma das matrizes em uma unica
+        for (int octave = octaveCount - 1; octave >= 0; octave--){
+
+
+            amplitude *= persistence;
+            totalAmplitude += amplitude;
+
+            for (int x = 0; x < width; x++){
+                for (int y = 0; y < height; y++){
+                    //Soma as matrizes em uma unica
+                    perlinNoise[x,y] += smoothNoise[octave, x, y] * amplitude;
+                }
+            }
+        }
+
+        //normalização (traz os valores de volta para o intervalo de 0 a 1
+        for (int x = 0; x < width; x++){
+            for (int y = 0; y < height; y++){
+                perlinNoise[x, y] /= totalAmplitude;
+            }
+        }
+
+        return perlinNoise;
+    }
+
+
+    //Gera uma matriz com ruido aleatório (usando uma seed, uma mesma seed sempre gera o mesmo resultado)
+    static float [,] GenerateWhiteNoise(int w, int h, int seed){
+
+        System.Random rand = new System.Random(seed);
+        
+        float[,] noise = new float[w,h];
+        for (int y = 0; y < h; y++){
+            for (int x = 0; x < w; x++) {
+                noise[x, y] = rand.Next();
+            }
+        }
+        return noise;
+    }
+
+    static float[,] GenerateSmoothNoise(float[,] baseNoise, int octave){
+
+        int width = baseNoise.GetLength(0);
+        int height = baseNoise.GetLength(1);
+
+        float[,] smoothNoise = new float[width, height];
+
+        int samplePeriod = 1 << octave;//"move" o "1" para a esquerda. ex: octave = 1:  1 << 1 -> 10(binário) = 2
+                                       //ex2: octave = 2: 1 << 2 -> 100(binário) = 4
+                                       //ex3: octave = 3: 1 << 3 -> 1000(binário) = 8
+        float sampleFrequency = 1.0f / samplePeriod;
+
+        //Sinceramente não faço ideia do que essa maluquice abaixo faz
+        for (int i = 0; i < width; i++){
+            int sample_i0 = (i / samplePeriod) * samplePeriod;
+            int sample_i1 = (sample_i0 + samplePeriod) % width;
+            float horizontal_blend = (i - sample_i0) * sampleFrequency;
+
+            for (int j = 0; j < height; j++){
+                int sample_j0 = (j / samplePeriod) * samplePeriod;
+                int sample_j1 = (sample_j0 + samplePeriod) % height;
+                float vertical_blend = (j - sample_j0) * sampleFrequency;
+
+
+                float top = Mathf.Lerp(baseNoise[sample_i0, sample_j0], baseNoise[sample_i1, sample_j0], horizontal_blend);
+                float bottom = Mathf.Lerp(baseNoise[sample_i0, sample_j1], baseNoise[sample_i1, sample_j1], horizontal_blend);
+
+
+                smoothNoise[i, j] = Mathf.Lerp(top, bottom, vertical_blend);
+            }
+        }
+
+        return smoothNoise;
+    }
+}
--- Assets/Vaporwave&Eurobeats/Scripts/World/World.cs
+++ Assets/Vaporwave&Eurobeats/Scripts/World/World.cs
@@ -2,15 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class World : MonoBehaviour {
+public enum BlockMaterial : byte {
+    Empty,
+    Solid
+}
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public abstract class WorldGenerator : ScriptableObject {
+    public abstract void Generate(World world);
+}
+
+public class Chunk {
+
+    public BlockMaterial[,,] blocks;
+
+    public Chunk(byte chunkSize, byte chunkHeight) {
+        blocks = new BlockMaterial[chunkSize, chunkHeight, chunkSize];
+    }
+}
+
+public class PerlinNoiseGenerator : WorldGenerator {
+    public override void Generate(World world) {
+
+        var size = world.Width * world.ChunkSize;
+        var noise = PerlinNoise.Generate(size, size, 8, 0);
+        for (byte i = 0; i < world.Width; i++) {
+            for (byte j = 0; j < world.Height; j++) {
+                var chunk = new Chunk(world.ChunkSize, world.ChunkHeight);
+                
+                FillChunk(world, chunk, noise, i, j);
+            }
+        }
+    }
+
+    private static void FillChunk(World world, Chunk chunk, float[,] noise, byte i, byte j) {
+        var endX = world.ChunkSize * i + world.ChunkSize;
+        var endZ = world.ChunkSize * j + world.ChunkSize;
+
+        var startX = endX - world.ChunkSize;
+        var startZ = endZ - world.ChunkSize;
+
+        for (var x = startX; x < endX; x++) {
+            for (var z = startZ; z < endX; z++) {
+                var height = (int)(world.ChunkHeight * noise[x, z]);
+                
+                for (var y = 0; y < world.ChunkHeight; y++) {
+                    if (y < height)
+                        chunk.blocks[x, y, z] = BlockMaterial.Solid;
+                    else 
+                        chunk.blocks[x, y, z] = BlockMaterial.Empty;                  
+                }
+            }
+        }
+    }
+}
+
+public class World : MonoBehaviour {
+    //Quantos chunks
+    public byte Width;
+
+    //Quantos chunks
+    public byte Height;
+    public byte ChunkSize;
+    public byte ChunkHeight;
 }
